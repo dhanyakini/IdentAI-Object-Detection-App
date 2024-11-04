930cd1b2b5c0f2c76bb47bb6ed621cd2b4bc8438
Dhanya Kini <dhanyakini@gmail.com>
2024-11-04 20:38:17 +0530

    Initial Commit

---
+++ IdentAITests/IdentAITests.swift
@@ -0,0 +1,17 @@
+//
+//  IdentAITests.swift
+//  IdentAITests
+//
+//  Created by Dhanya Kini on 04/11/24.
+//
+
+import Testing
+@testable import IdentAI
+
+struct IdentAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
